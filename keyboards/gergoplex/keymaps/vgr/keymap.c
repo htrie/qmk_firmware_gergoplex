ed1288c5f8b60ceccc5190051d8bac6cdd3ede74
vgr <vensan@me.com>
2020-04-30 22:06:56 +1200

    update

--- keyboards/gergoplex/keymaps/vgr/keymap.c
+++ keyboards/gergoplex/keymaps/vgr/keymap.c
@@ -24,27 +24,27 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_gergoplex(
-    KC_Q,               KC_W,               KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,               KC_P, 
-    MT(MOD_LCTL, KC_A), MT(MOD_LSFT, KC_S), KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    MT(MOD_RSFT, KC_L), LT(ARRW, KC_SCLN),
-    KC_Z,               KC_X,               KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,             KC_SLSH,
-                       KC_LGUI, MT(MOD_RCTL, KC_NO), MO(SYMB),  KC_SPC, MO(NUMB), MT(MOD_RALT, KC_NO)
+    KC_Q,               KC_W,               KC_E, KC_R, KC_T,    KC_Y, KC_U, KC_I,    KC_O,               KC_P, 
+    MT(MOD_LCTL, KC_A), MT(MOD_LSFT, KC_S), KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    MT(MOD_RSFT, KC_L), LT(ARRW, KC_SCLN),
+    KC_Z,               KC_X,               KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT,             KC_SLSH,
+                     MO(NUMB), MT(MOD_LCTL, KC_NO), MO(SYMB),    KC_SPC, MT(MOD_LALT, KC_NO), KC_LGUI
     ),
 [SYMB] = LAYOUT_gergoplex(
-    KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,   KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_AMPR,   KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
-    KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,
-                      XXXXXXX, XXXXXXX, KC_TRNS,   KC_EQL,  XXXXXXX, XXXXXXX
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LPRN,    KC_MINS, KC_7, KC_8, KC_9, KC_PLUS,
+    KC_PIPE, KC_PERC, KC_AMPR, KC_RPRN, KC_LCBR,    KC_0,    KC_4, KC_5, KC_6, KC_ASTR,
+    KC_CIRC, KC_TILD, KC_QUOT, KC_DQUO, KC_LBRC,    KC_DOT,  KC_1, KC_2, KC_3, KC_BSLS,
+                      XXXXXXX, XXXXXXX, KC_TRNS,    KC_EQL,  KC_TRNS, KC_TRNS
     ),
 [NUMB] = LAYOUT_gergoplex(
-    KC_F1, KC_F2,  KC_F3,   KC_F4,   XXXXXXX,     KC_PPLS, KC_7, KC_8, KC_9, KC_PMNS,
-    KC_F5, KC_F6,  KC_F7,   KC_F8,   XXXXXXX,     KC_0,    KC_4, KC_5, KC_6, KC_PAST,
-    KC_F9, KC_F10, KC_F11,  KC_F12,  XXXXXXX,     KC_DOT,  KC_1, KC_2, KC_3, KC_PSLS,
-                   XXXXXXX, KC_TRNS, XXXXXXX,     XXXXXXX, KC_TRNS, KC_TRNS
+    KC_F1, KC_F2,  KC_F3,   KC_F4,   XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_F5, KC_F6,  KC_F7,   KC_F8,   XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_F9, KC_F10, KC_F11,  KC_F12,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                   XXXXXXX, KC_TRNS, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX
     ),
 [ARRW] = LAYOUT_gergoplex(
-    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_PGUP,   KC_HOME, KC_UP,   KC_END, XXXXXXX,
-    KC_TRNS,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,
-    KC_TRNS,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                      XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX,             XXXXXXX,             XXXXXXX,             XXXXXXX, KC_RPRN,     KC_PGUP,   KC_HOME, KC_UP,   KC_END,  XXXXXXX,
+    MT(MOD_LCTL, KC_NO), MT(MOD_LSFT, KC_NO), MT(MOD_LALT, KC_NO), KC_TAB,  KC_RCBR,     KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,
+    XXXXXXX,             KC_TRNS,             KC_TRNS,             KC_TRNS, KC_RBRC,     XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                                         XXXXXXX,  KC_TRNS, XXXXXXX,     XXXXXXX,   KC_TRNS, XXXXXXX
     )
 };
